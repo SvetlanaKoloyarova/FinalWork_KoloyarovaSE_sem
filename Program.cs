722de105eb4e450786a9392bd2b09165c7133333
SvetlanaKoloyarova <103605545+SvetlanaKoloyarova@users.noreply.github.com>
2023-10-20 09:35:54 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Задача 2. Напишите псевдокод по блок-схеме
+// Найти среднее арифметическое среди всех элементов массива [2, 5, 13, 7, 6, 4]
+
+numbers = [2, 5, 13, 7, 6, 4]
+size = 6
+sum = 0
+avg = 0
+index = 0
+while index < size do 
+    sum = sum + numbers[index]
+    index = index + 1
+avg = sum / size
+print(avg)
